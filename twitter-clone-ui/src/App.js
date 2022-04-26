24e11cc9a1cf45f2c89ec87b35165b81ac902ae1
arielmdc <arielmcarvalho@gmail.com>
2022-04-26 18:52:12 -0300

    feat: add post componets, feed and trending grid

--- twitter-clone-ui/src/App.js
+++ twitter-clone-ui/src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import Sidebar from './Components/Sidebar';
+import Post from './Components/Post';
+import SearchIcon from '@mui/icons-material/Search';
 
 function App() {
   return (
@@ -8,9 +10,26 @@ function App() {
         <Sidebar />
       </div>
       <div className="feed-grid">
-
+        <div className="post">
+          <Post />
+        </div>
+        <div className="feed-news">
+          
+        </div>
+        
       </div>
       <div className="trending-grid">
+        
+        <div className="search">
+          <input type="text" placeholder="Search Twitter"></input>
+        </div>
+
+        <div className="trending-fake-box">
+          What’s happening
+        </div>
+        <div className="trending-fake-box">
+          Who to follow
+        </div>
 
       </div>
       
