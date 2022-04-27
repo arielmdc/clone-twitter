2fee072774f47692e5a0ddfb30b783fe4309a6ba
arielmdc <arielmcarvalho@gmail.com>
2022-04-27 02:44:47 -0300

    feat: search recent tweets

--- twitter-clone-api/index.js
+++ twitter-clone-api/index.js
@@ -1,12 +1,28 @@
-const express = require("express");
-const cors = require("cors");
-const server = express();
-server.use(cors());
+const Twit = require('twit');
+require('dotenv').config()
 
-server.post("/usuarios", (req,res)=>{
-    return res.json({usuario: "ariel"})
+const T = new Twit({
+    consumer_key: process.env.consumer_key,
+    consumer_secret: process.env.consumer_secret,
+    access_token: process.env.access_token,
+    access_token_secret: process.env.access_token_secret,
 });
 
-server.listen(3001, () =>{
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.post("/usuarios", (req,res)=>{
+    const tag = req.body.searchTag;
+    console.log(tag)
+    T.get('search/tweets', { q: tag, count: 2 }, function(err, data, response) {
+        return res.json(data.statuses)
+      })
+});
+
+app.listen(3001, () =>{
  console.log("Servidor ta funcionando");
 });
