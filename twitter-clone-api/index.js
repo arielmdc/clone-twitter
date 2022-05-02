baafe990bd5c80e18fbc9e567a6d5e62a6f7bcdb
arielmdc <arielmcarvalho@gmail.com>
2022-05-02 18:13:06 -0300

    feat: converted text without tags

--- twitter-clone-api/index.js
+++ twitter-clone-api/index.js
@@ -18,15 +18,20 @@ app.use(express.json());
 app.post("/tags", (req,res)=>{
     const tag = req.body.searchTag;
     console.log(tag)
-    T.get('search/tweets', { q: tag, count: 10, tweet_mode: 'extended', exclude: "retweets"}, function(err, data, response) {
+    T.get('search/tweets', { q: tag, count: 20, tweet_mode: 'extended', exclude: "retweets"}, function(err, data, response) {
         resposta = [];
         data.statuses.map((val,key)=>{
+            full_text_converted = val.full_text.split(" ");
             hashtags = "";
             media_type = "";
             media_url = "";
             val.entities.hashtags.map((val2,key)=>{
                 hashtags+= "#"+val2.text+" ";
+                full_text_converted = full_text_converted.filter(v => v !== "#"+val2.text);
             })
+            full_text_converted = full_text_converted.join(" ");
+            console.log(full_text_converted, hashtags);
+            // console.log(full_text_converted);
             if(typeof val.entities.media != "undefined"){
                 if(val.extended_entities.media['0'].type != "video"){
                     media_type = "photo"
@@ -41,10 +46,10 @@ app.post("/tags", (req,res)=>{
                     })
                 }
             }
-            resposta.push({"full_text": val.full_text,"hashtags": hashtags, "retweet_count": val.retweet_count, "favorite_count": val.favorite_count,
+            resposta.push({"full_text": full_text_converted,"hashtags": hashtags, "retweet_count": val.retweet_count, "favorite_count": val.favorite_count,
              "profile_image_url": val.user.profile_image_url, "user_name": val.user.name, "screen_name": val.user.screen_name,
             "media_type": media_type,  "media_url":  media_url})
-            console.log(resposta)
+            // console.log(resposta)
             // console.log(y)
             // console.log(val.full_text)
             // x.push([val.full_text])
